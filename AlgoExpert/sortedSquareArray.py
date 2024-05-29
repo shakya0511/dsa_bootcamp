ac673f20708e7645fee5c7180e34267dd39953be
Himanshu shakya <39743800+himanshushakya51195@users.noreply.github.com>
2024-05-29 09:57:06 +0530

    Commit C010
    - AlgoExpert

--- AlgoExpert/sortedSquareArray.py
+++ AlgoExpert/sortedSquareArray.py
@@ -12,52 +12,32 @@ def Solution1(array):
 
     return SortedArray
 
-    
-
-
+     
 
 
 def Solution2(array):
 
-    zeroArray =  [0 for _ in range(len(array))]
+    SortedArray =  [0 for _ in range(len(array))]
 
-    Idx1 = 0
-    Idx2 = len(array) - 1
-    ZeroIdx = len(zeroArray) - 1
+    smallerIdx = 0
+    largerIdx = len(array) - 1
 
-    # Learning : deal with negative integers saperately 
+    for idx in reversed(range(len(array))):
+        smallerIdxValue = array[smallerIdx]
+        largerIdxValue = array[largerIdx]
 
-    while Idx1 < len(array) -1 :
-        sqr1 = (array[Idx1] * array[Idx1])
-        sqr2 = (array[Idx2] * array[Idx2])
+        if abs(smallerIdxValue) > abs(largerIdxValue):
+            SortedArray[idx] = smallerIdxValue * smallerIdxValue
+            smallerIdx += 1
+        else:
+            SortedArray[idx] = largerIdxValue * largerIdxValue
+            largerIdx -= 1
 
-        if sqr1 < sqr2:
-                zeroArray[ZeroIdx]= sqr2
-                Idx2 -= 1
-                ZeroIdx -= 1
-
-        if sqr1 > sqr2:
-
-            if array[Idx1] and array[Idx2] > 0:   
-                zeroArray[ZeroIdx] = sqr1
-                Idx1 += 1
-                ZeroIdx -= 1            
-            if array[Idx1] and array[Idx2] < 0:
-                zeroArray[ZeroIdx]= sqr1
-                Idx1 += 1
-                ZeroIdx -= 1
-
-    return zeroArray
-
-    
+    return SortedArray
 
 
 
 
-
-
-# array = [1,2,3,5,6,8,9]
-# array = [-2, -1]
+# problem
 array = [-7,-5,-4,3,6,8,9]
-
 Solution1(array)
