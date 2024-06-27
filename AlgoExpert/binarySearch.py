73ab4ca2359d28f94ad02e8bd02efefe8a345646
Himanshu Shakya <himanshu.shakya.official@outlook.com>
2024-06-27 13:08:16 +0530

    Commit C019
    AlgoExpert

--- AlgoExpert/binarySearch.py
+++ AlgoExpert/binarySearch.py
@@ -1,53 +1,21 @@
-# create 3 pointer : start, end , mid 
-# mid is sum of start index end index modulo 2 
-# calculate mid 
-# if mid is smaller than look up value then make start pointer = 0 end pointer = (mid - 1)
-# if mid is larger than look up value then make start pointer = (mid + 1) and end pointer len(array)
-# calulate mid point compare it with lookup value 
-# repeat step 4 
-# repeat step 5 
-# if index of small pointer is greater than large pointer than lookup value not found in array
-# assumption over here is the given array is sorted
+arr = [1,2,3,4,5,6,7,8,9,10]
+start = 0
+end = len(arr) - 1
+target = 2
 
+print(f"len is {len(arr)}")
 
-def Solution1(array, target):
-    smaller_pointer = 0
-    larger_pointer = len(array) - 1
+def binarySearch(arr, start, end, target):
+    print(f"array is : {arr},start point is : {start},end point is : {end},target is : {target}")
     
-    while smaller_pointer <= larger_pointer:
-        midIDx = (smaller_pointer + larger_pointer) // 2
-        if target < array[midIDx]:
-            larger_pointer = (midIDx - 1)
-
-        if target > array[midIDx]:
-            smaller_pointer = (midIDx + 1)
-
-        if target == array[midIDx]:
-            return midIDx
-    
-    return -1
-
-
-def Solution2 (array, target):
-    
-    return helperFunc (array, target, 0, len(array) -1)
-
-def helperFunc (array, target, left, right):
-        
-        if left > right:
-             return -1
-        midIdx = (left + right) // 2
-        if target == array[midIdx]:
-             return midIdx 
-        elif target < array[midIdx]:
-             return helperFunc(array, target, left, midIdx - 1 )
+    while start < end:
+        midIndex = (start+end)//2
+        if arr[midIndex] == target:
+            return print(f'target value {target} found')
+        elif arr[midIndex] > target:
+            return binarySearch(arr, start, midIndex-1, target)
         else:
-             return helperFunc(array, target, midIdx + 1, right)
-             
+            return binarySearch(arr, midIndex+1, end, target)
     
 
-array = [0, 22, 23, 34, 54, 44, 90]
-target = 44
-
-Solution2(array, target)
-print("run")
+binarySearch(arr, start, end, target)
